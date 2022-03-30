fb6262a89250b11f2c0c5c9a57aea68bd859a6ce
Víctor Míguez <victor@redradix.com>
2022-03-30 09:29:55 +0200

    Clean and improve initial setup.
    Remove web-vitals dependency.
    Update some other dependencies.
    Improve test description.
    Add .nvmrc file.
    Improve README explaining why the warning about items with duplicated keys is present.

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-  it('fails', () => {
+  it('displays "no results" message while having no movies', () => {
     render(<App />)
 
     expect(screen.getByText('No results yet')).toBeInTheDocument()
