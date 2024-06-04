97396538c963c0de06d065dab782e68e25fe71f6
Víctor Míguez <victor@redradix.com>
2024-06-04 07:22:29 +0200

    Update versions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import { createRoot } from 'react-dom/client'
 import React from 'react'
-import ReactDOM from 'react-dom'
+
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
+const domNode = document.getElementById('root')
+const root = createRoot(domNode)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
